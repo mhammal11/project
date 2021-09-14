ed007f77b9b50d6fb7d57f2188c0310575678a0e
Michael Hammal <hammalm@mcmaster.ca>
2021-09-13 23:45:29 -0400

    Add simple products with simple styling. Add shopping cart button.

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import Grid from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
+import Product from './Product/Product'
+
+const products = [
+    { id: 150, name: 'The Power Prenatal (for eggs)', description: 'Sample Description.', price: '$60'},
+    { id: 151, name: 'The Power Prenatal (for sperm)', description: 'Sample Description.', price: '$40'},
+];
 
 const Products = () => {
-    <main>
-        <Grid container justify="center" spacing={4}>
-
-        </Grid>
-    </main>
+    return (
+        <main>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product) => (
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    )
+    
 }
 
 export default Products;
