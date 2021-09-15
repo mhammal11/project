ba684808b799ece43707309dcb2cab9b82a6bf4b
Michael Hammal <hammalm@mcmaster.ca>
2021-09-15 19:25:49 -0400

    Add radio selections and change font colour.

--- src/components/products/Product/Product.jsx
+++ src/components/products/Product/Product.jsx
@@ -6,26 +6,32 @@ import { mergeClasses } from '@material-ui/styles';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
+
 import useStyles from './styles';
 
 const Product = ({ product }) => {
     const classes = useStyles();
     const [expanded, setExpanded] = React.useState(false);
-
+    console.log(product)
     const handleExpandClick = () => {
         setExpanded(!expanded);
     };
+
+    const onChangeValue = (event) => {
+        const listen = true;
+        console.log(event.target.value);
+      }
 
     return (
         <Card className={classes.root} >
             <CardMedia className={classes.media} image={product.primary_image.url_standard} title={product.name} onClick={handleExpandClick}/>
             <CardContent onClick={handleExpandClick}>
                 <div className={classes.cardContent}>
-                    <Typography variant="h5"  justify="flex-start" gutterBottom>
+                    <Typography variant="h5"  color="primary" justify="flex-start" gutterBottom>
                         {product.name}
                     </Typography>
-                    <Typography variant="h6">
-                        <div>${product.price}</div>
+                    <Typography variant="h6" color="secondary">
+                        <div >${product.price}</div>
                     </Typography>
                 </div>
                 <Typography variant="body2" color="textSecondary">{product.meta_description}</Typography>
@@ -34,6 +40,16 @@ const Product = ({ product }) => {
                 <IconButton aria-label="Add to Cart">
                     <AddShoppingCart />
                 </IconButton>
+                <Typography fontFamily="inherit">
+                <div onChange={onChangeValue}>
+                <label >
+                    <input type="radio" value={product.modifiers[1].option_values[0].label} name="Delivery Frequency" required /> {product.modifiers[1].option_values[0].label}
+                </label>
+                <label >
+                    <input type="radio" value={product.modifiers[1].option_values[1].label} name="Delivery Frequency" /> {product.modifiers[1].option_values[1].label}
+                </label>
+                </div>
+                </Typography>
                 <IconButton
                     className={clsx(classes.expand, {
                     [classes.expandOpen]: expanded,
